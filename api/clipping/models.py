adc7b3d6c28466ec8545be31203bfd1e9cda1003
Matheus Martins <matheus.mmag@gmail.com>
2018-11-29 23:05:28 -0200

    Models

--- api/clipping/models.py
+++ api/clipping/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Autor(models.Model):
+	nome = models.TextField('Nome')
+	data_criacao = models.DateTimeField(auto_now_add=True)
+	data_atualizacao = models.DateTimeField(auto_now=True)
+
+	def __str__(self):
+		return self.nome
+
+	class Meta:
+		verbose_name = 'Autor'
+		verbose_name_plural = 'Autores'
+
+class Livro(models.Model):
+	titulo = models.TextField('Título')
+	autor = models.ForeignKey(Autor, on_delete=models.PROTECT, related_name='livro', verbose_name='Autor')
+	data_criacao = models.DateTimeField(auto_now_add=True)
+	data_atualizacao = models.DateTimeField(auto_now=True)
+
+	def __str__(self):
+		return self.titulo
+
+	class Meta:
+		verbose_name = 'Livro'
+		verbose_name_plural = 'Livros'
+
+class Recorte(models.Model):
+	texto = models.TextField('Recorte')
+	data_criacao = models.DateTimeField(auto_now_add=True)
+	data_atualizacao = models.DateTimeField(auto_now=True)
+	posicao = models.TextField('Posição/Página', null=True, blank=True)
+
+	user = models.ForeignKey(User, on_delete=models.PROTECT, related_name='recorte', verbose_name='Usuario')
+	livro = models.ForeignKey(Livro, on_delete=models.PROTECT, related_name='recorte', verbose_name='Livro')
+	autor = models.ForeignKey(Autor, on_delete=models.PROTECT, related_name='recorte', verbose_name='Autor')
+
+	class Meta:
+		verbose_name = 'Recorte'
+		verbose_name_plural = 'Recortes'
--- api/clipping/views.py
+++ api/clipping/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
+from django.contrib.auth.models import User
+
 # Create your views here.
