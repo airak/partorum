28a40a6b196f858f51f70e3a558fe57a2f63efbd
analuizatrz <analuizatrz@gmail.com>
2018-12-01 21:16:21 -0200

    add bootstrap

--- api/api/settings.py
+++ api/api/settings.py
@@ -40,7 +40,10 @@ INSTALLED_APPS = [
 
     'clipping',
     'core',
+    'crispy_forms',
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
